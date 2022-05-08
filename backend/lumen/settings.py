55881c586af15cc363fdb99373c6d04ff397be6b
Ana <ana.grskovic56@gmail.com>
2022-05-08 19:02:21 +0200

    Removed team and team member names.

--- backend/lumen/settings.py
+++ backend/lumen/settings.py
@@ -131,5 +131,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'lil/build/static')
+    os.path.join(BASE_DIR, 'frontend/build/static')
 ]
